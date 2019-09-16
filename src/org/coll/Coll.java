634ab65313c8920d2a990b68a7d43f568fe1100e
vpriya2209 <priyavishnu621@gmail.com>
2019-09-16 13:55:43 +0530

    success

---
+++ src/org/coll/Coll.java
@@ -0,0 +1,16 @@
+package org.coll;
+
+
+import java.util.*;
+
+public class Coll {
+public static void main(String[] args) {
+	List li=new ArrayList();
+	li.add(10);
+	li.add(20);
+	System.out.println(li);
+}
+	
+	
+	
+}
